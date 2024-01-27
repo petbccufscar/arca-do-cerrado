9e4f87d79fe4876072e0bd244cc342206b9e7aa8
jorgeprj <jlmp2002@gmail.com>
2024-01-27 04:25:00 -0300

    feat: specie page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { BrowserRouter as Router } from "react-router-dom"
 import { AppRoutes } from "./routes"
 import Navbar from "./components/layout/navbar/Navbar"
 import Footer from "./components/layout/footer/Footer"
+import { register } from 'swiper/element/bundle'
+
+register()
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 
 function App() {
 
--- src/pages/species/specie/Specie.jsx
+++ src/pages/species/specie/Specie.jsx
@@ -1,9 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-
-import './Specie.css';
 import { FaSeedling } from 'react-icons/fa6';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+const Header = ({ specie }) => {
+    return (
+        <header>
+            <div className='h-[300px] w-[300px]'>
+                <Swiper
+                    slidesPerView={1}
+                    pagination={{ clickable: true }}
+                    navigation
+                >
+                    {specie.imagens.map((imagem, index) => (
+                        <SwiperSlide key={index}>
+                            <img src={imagem.imagem} alt="slide-item" className='h-[300px] w-[300px] object-cover' />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </header>
+    )
+}
 
 const Specie = () => {
     const { id } = useParams();
@@ -31,33 +52,20 @@ const Specie = () => {
     }
 
     return (
-        <div className='specie'>
-            <section className='main'>
-                <section className='header'>
-                    <h2>{specie.apelido}</h2>
-                    <p><FaSeedling /> {specie.nome_cientifico}</p>
-                    <p>{specie.resumo}</p>
-                </section>
-                <section className='content'>
-                        {specie.imagens.map((imagem, index) => (
-                            <div key={index}>
-                                <img src={imagem} alt={`Imagem ${index + 1}`} />
-                            </div>
-                        ))}
-                    <div className='text'>
-                        <h3>Description</h3>
+        <div>
+            <h1 className='bg-primary-color p-4 text-white text-center text-3xl font-semibold'>{specie.apelido}</h1>
+            <main className='flex flex-col sm:flex-row py-8 px-6 mx-auto max-w-screen-xl lg:px-8 gap-8'>
+                <Header specie={specie}/>
+                <section>
+                    <h2 className='text-2xl font-semibold'>{specie.apelido}</h2>
+                    <p className='flex items-center gap-2 text-neutral-500'><FaSeedling/> {specie.nome_cientifico}</p>
+                    <div className='flex flex-col mt-4 gap-2'>
+                        <h3 className='text-xl'>Descrição</h3>
                         <p>{specie.texto}</p>
                     </div>
+
                 </section>
-                <section className='Footer'>
-                    <div className='link-at-the-end'>
-                        <p>
-                            <strong>Informações adicionais:</strong>{' '}
-                            <a href={specie.link} target='_blank' rel='noopener noreferrer'> Mais informações</a>
-                        </p>
-                    </div>
-                </section>
-            </section>
+            </main>
         </div>
     );
 };
