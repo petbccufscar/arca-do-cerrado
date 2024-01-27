e7693dcc1d7dd18d3875c359844b055a78cb70bf
jorgeprj <jlmp2002@gmail.com>
2024-01-27 02:52:54 -0300

    fix: warnings

--- src/components/layout/form/ContributeForm.jsx
+++ src/components/layout/form/ContributeForm.jsx
@@ -14,15 +14,15 @@ const ContributeForm = () => {
                 </p>
                 <form action="#" className="space-y-8">
                     <div>
-                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Seu email</label>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Seu email</label>
                         <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="name@arcadocerrado.com" required />
                     </div>
                     <div>
-                        <label for="subject" className="block mb-2 text-sm font-medium text-gray-900">Assunto</label>
+                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900">Assunto</label>
                         <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Assunto da mensagem" required />
                     </div>
                     <div className="sm:col-span-2">
-                        <label for="message" className="block mb-2 text-sm font-medium text-gray-900">Sua mensagem</label>
+                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Sua mensagem</label>
                         <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Escreva sua mensagem..."></textarea>
                     </div>
                     <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit bg-primary-color focus:ring-4 focus:outline-none focus:ring-primary-300">Enviar mensagem</button>
