51c08ce184cdb160a2d1add2396d95625d6da766
JoaoOLM <joao.moraes@estudante.ufscar.br>
2024-02-15 20:18:35 -0300

    Algumas correções simples solicitadas na última reunião

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Post from '../../components/blog/Post'
+import axios from 'axios';
 
 const Blog = () => {
+
+    const [postsData, setPostsData] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8000/api/Postagem`)
+            .then(response => {
+                setPostsData(response.data)
+            })
+            .catch(error => {
+                console.error('Erro ao buscar dados das postagens:', error);
+            });
+    }, []);
 
     const posts = [
         {
@@ -25,24 +38,24 @@ const Blog = () => {
     ]
 
     const navigate = useNavigate();
-    const goToSubscribe = () => navigate ('/inscrever')
+    const goToSubscribe = () => navigate('/inscrever')
 
     return (
         <div className='flex flex-col min-h-screen w-full items-center'>
             <h1 className='bg-primary-color p-4 text-white text-center text-xl sm:text-3xl font-semibold w-full'>Blog</h1>
             <section className='flex flex-col py-8 px-6 mx-auto max-w-screen-xl lg:px-8 justify-center items-center'>
-                <h2 className='text-2xl font-semibold mb-4 border-b-2 border-primary-color max-w-fit px-2'>Últimas Notícias</h2>
+                <section className='flex flex-col justify-center items-center mt-10 mb-10'>
+                    <h2>Inscreva-se para receber mais notificações</h2>
+                    <button className='mt-4 text-sm' onClick={goToSubscribe}>Inscrever-se</button>
+                </section>
+
                 <section className='flex flex-col gap-8 mt-8'>
-                    {posts
+                    {postsData
                         .reverse()
                         .map(post => (
                             <Post key={post.id} post={post} />
                         ))}
                 </section>
-                <section className='flex flex-col justify-center items-center mt-20'>
-                    <h2>Inscreva-se para receber mais notificações</h2>
-                    <button className='mt-4 text-sm' onClick={goToSubscribe}>Registre-se</button>
-                </section>  
             </section>
         </div>
 
--- src/pages/team/Team.jsx
+++ src/pages/team/Team.jsx
@@ -1,51 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Person = ({ personName, personImage, personRole, personId }) => {
-	return (
-		<div className="text-center text-gray-5000">
-			<a href={`/equipe/${personId}`} className='cursor-pointer'>
-				<img className="mx-auto mb-4 w-36 h-36 rounded-full object-cover hover:opacity-85" src={personImage} alt="Person photo" />
-			</a>
+const Person = ({ personName, personImage, personId }) => {
+    return (
+        <div className="text-center text-gray-500">
+            <a href={`/equipe/${personId}`} className='cursor-pointer'>
+                <img className="mx-auto mb-4 w-36 h-36 rounded-full object-cover hover:opacity-85" src={personImage} alt="Person photo" />
+            </a>
 
-			<h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900">
-				<a href={`/equipe/${personId}`}>{personName}</a>
-			</h3>
-			<p>{personRole}</p>
-		</div>
-	);
+            <h3 className="mb-1 text-lg font-semibold tracking-tight text-gray-900">
+                <a href={`/equipe/${personId}`}>{personName}</a>
+            </h3>
+        </div>
+    );
 };
 
 const Team = () => {
-	const [teamData, setTeamData] = useState([]);
+    const [teamData, setTeamData] = useState([]);
 
-	useEffect(() => {
-		// Faça a solicitação para obter dados da equipe do seu backend aqui
-		axios.get(`http://127.0.0.1:8000/api/Equipe`)
-			.then(response => {
-				setTeamData(response.data);
-			})
-			.catch(error => {
-				console.error('Erro ao buscar dados da equipe:', error);
-			});
-	}, []); // O array vazio como segundo argumento significa que este efeito só é executado uma vez, equivalente ao componentDidMount
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8000/api/Equipe`)
+            .then(response => {
+                setTeamData(response.data);
+            })
+            .catch(error => {
+                console.error('Erro ao buscar dados da equipe:', error);
+            });
+    }, []);
 
-	return (
-		<div>
-			<h1 className='bg-primary-color p-4 text-white text-center text-xl sm:text-3xl font-semibold'>Quem somos</h1>
-			<section>
-				<div className='flex flex-col py-8 px-6 mx-auto max-w-screen-xl lg:px-8  items-center'>
-					<h2 className='text-2xl font-semibold mb-8 border-b-2 border-primary-color max-w-fit px-4 text-center'>Equipe</h2>
-					<div className='grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-						{/* Mapear e renderizar informações dos colaboradores aqui */}
-						{teamData.map((person, index) => (
-							<Person key={index} personId={person.id} personName={person.nome} personLink={person.link} personImage={person.imagem} personRole={person.cargo} />
-						))}
-					</div>
-				</div>
-			</section>
-		</div>
-	);
+    const coordenador = teamData.find(person => person.cargo === 'Coordenador');
+    const equipe = teamData.filter(person => person.cargo !== 'Coordenador');
+
+    return (
+        <div>
+            <h1 className='bg-primary-color p-4 text-white text-center text-xl sm:text-3xl font-semibold'>Quem somos</h1>
+            <section className='py-8 px-6 mx-auto max-w-screen-xl lg:px-8  items-center'>
+                <h2 className='text-2xl font-semibold mb-8 border-b-2 border-primary-color max-w-fit px-4 text-center mx-auto'>Coordenador</h2>
+                <div className='mx-auto mb-8 w-36 h-36'>
+                    {coordenador && <Person personId={coordenador.id} personName={coordenador.nome} personImage={coordenador.imagem} personRole={coordenador.cargo} />}
+                </div>
+                <div className='mb-20'></div> 
+                <h2 className='text-2xl font-semibold mb-8 border-b-2 border-primary-color max-w-fit px-4 text-center mx-auto'>Equipe</h2>
+                <div className='grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+                    {equipe.map((person, index) => (
+                        <Person key={index} personId={person.id} personName={person.nome} personImage={person.imagem} />
+                    ))}
+                </div>
+            </section>
+        </div>
+    );
 };
 
 export default Team;
