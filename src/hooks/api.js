24ed3eb392d48b87cb6893a350a0bee25166a71e
JoaoOLM <joao.moraes@estudante.ufscar.br>
2024-06-27 19:47:22 -0300

    Mudando url do backend

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://arca-do-cerrado.onrender.com/api';
 const token = import.meta.env.VITE_TOKEN;
 
 export const handleResponse = async (response, errorMessage) => {
