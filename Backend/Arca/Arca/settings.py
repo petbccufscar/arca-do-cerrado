ff849ea193bd621cbe6b08a60cbb5770046d36ea
JoaoOLM <joao.moraes@estudante.ufscar.br>
2024-06-27 17:59:10 -0300

    Change allowed hosts

--- Backend/Arca/Arca/settings.py
+++ Backend/Arca/Arca/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'arca-do-cerrado.onrender.com',
+]
 
 CORS_ALLOWED_ORIGINS = os.getenv('DJANGO_CORS_ALLOWED_ORIGINS', '').split(',')
 
