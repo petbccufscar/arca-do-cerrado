31188566fbbc2fda106f0722264f42d6c68620e5
JoaoOLM <joao.moraes@estudante.ufscar.br>
2024-07-02 17:58:49 -0300

    Fix

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = 'https://arca-do-cerrado.onrender.com/api';
-const token = a4526eab0ac787f771aefa350414b0c8c9f813d0;
+const token = 'a4526eab0ac787f771aefa350414b0c8c9f813d0';
 
 export const handleResponse = async (response, errorMessage) => {
     if (!response.ok) {
