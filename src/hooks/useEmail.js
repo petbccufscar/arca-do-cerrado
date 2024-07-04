6321b4d99609c420be21399c1a579e3aa6d05d3d
JoaoOLM <joao.moraes@estudante.ufscar.br>
2024-07-04 18:00:20 -0300

    Debug++

--- src/hooks/useEmail.js
+++ src/hooks/useEmail.js
@@ -7,12 +7,14 @@ import AvisoMensagem from '../components/subscribe/email/folder/avisoMensagem.js
 // Função assíncrona para envio de emails com EmailJS
 const sendEmail = async (templateParams) => {
     try {
+        console.log(templateParams);
         const response = await emailjs.send(
             import.meta.env.VITE_EMAILJS_SERVICE_ID,
-            import.meta.env.VITE_EMAILJS_TEMPLATE_ID, // Corrigido de VITE_EMAILJS_TEMPLATE para VITE_EMAILJS_TEMPLATE_ID
+            import.meta.env.VITE_EMAILJS_TEMPLATE_ID, 
             templateParams,
             import.meta.env.VITE_EMAILJS_USER_ID
         );
+
         console.log('E-mail enviado com sucesso!', response.status, response.text);
     } catch (error) {
         console.error('Erro ao enviar e-mail:', error);
