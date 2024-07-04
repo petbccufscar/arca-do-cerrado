53aaa0658fa2c499e8c6267e2a4216fa5c96d2de
JoaoOLM <joao.moraes@estudante.ufscar.br>
2024-07-04 17:48:50 -0300

    Fix email send

--- src/hooks/useEmail.js
+++ src/hooks/useEmail.js
@@ -4,22 +4,27 @@ import AvisoInscricao from '../components/subscribe/email/folder/avisoInscricao.
 import ConfirmaInscricao from '../components/subscribe/email/folder/confirmaInscricao.jsx';
 import AvisoMensagem from '../components/subscribe/email/folder/avisoMensagem.jsx';
 
-{/*Função para envio de emails com EmailJS*/ }
+// Função para envio de emails com EmailJS
 const sendEmail = (templateParams) => {
-    emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE, templateParams, import.meta.env.VITE_EMAILJS_USER_ID)
-        .then((response) => {
-            console.log('E-mail enviado com sucesso!', response.status, response.text);
-        })
-        .catch((error) => {
-            console.error('Erro ao enviar e-mail:', error);
-        });
+    emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID, // Corrigido de VITE_EMAILJS_TEMPLATE para VITE_EMAILJS_TEMPLATE_ID
+        templateParams,
+        import.meta.env.VITE_EMAILJS_USER_ID
+    )
+    .then((response) => {
+        console.log('E-mail enviado com sucesso!', response.status, response.text);
+    })
+    .catch((error) => {
+        console.error('Erro ao enviar e-mail:', error);
+    });
 };
 
 const useEmail = () => {
 
     const sendAvisoInscricao = (nome) => {
         sendEmail({
-            toEmail: import.meta.env.VITE_EMAIL_ARCA,
+            to_email: import.meta.env.VITE_EMAIL_ARCA, 
             subject: 'Novo inscrito no blog!',
             message: AvisoInscricao({ name: nome }),
         });
@@ -27,7 +32,7 @@ const useEmail = () => {
 
     const sendConfirmaInscricao = (email, nome) => {
         sendEmail({
-            toEmail: email,
+            to_email: email, 
             subject: 'Obrigado por se inscrever',
             message: ConfirmaInscricao({ name: nome }),
         });
@@ -35,9 +40,9 @@ const useEmail = () => {
 
     const sendMensagem = (email, subject, mensagem) => {
         sendEmail({
-            toEmail: import.meta.env.VITE_EMAIL_ARCA,
+            to_email: import.meta.env.VITE_EMAIL_ARCA, 
             subject: subject,
-            message: AvisoMensagem( email, mensagem ),
+            message: AvisoMensagem(email, mensagem),
         });
         console.log(email);
     };
