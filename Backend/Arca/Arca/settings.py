b1edce92f30c12df5f4e386f2d6104b7ba71c4b3
JoaoOLM <joao.moraes@estudante.ufscar.br>
2025-03-12 11:32:27 -0300

    Email settings

--- Backend/Arca/Arca/settings.py
+++ Backend/Arca/Arca/settings.py
@@ -44,6 +44,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False').lower() == 'true'
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
